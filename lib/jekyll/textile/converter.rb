386d7a907d555bf6c5f7f2ecc18c9acd7c71304f
Parker Moore <parkrmoore@gmail.com>
2015-01-17 11:45:54 -0800

    Initial import.

---
+++ lib/jekyll/textile/converter.rb
@@ -0,0 +1,9 @@
+require "jekyll/textile/converter/version"
+
+module Jekyll
+  module Textile
+    module Converter
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/textile/converter/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Textile
+    module Converter
+      VERSION = "0.0.1"
+    end
+  end
+end
